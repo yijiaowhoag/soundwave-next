0351262212866238c4f9cc105981edf131888aa2
Yijiao Wang <ttwyj250@gmail.com>
2021-06-20 11:00:54 -0700

    Customize Spotify login button

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,43 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import { FaSpotify } from 'react-icons/fa';
+import Layout from '../components/Layout';
+
+const LoginButton = styled.button`
+  height: 48px;
+  margin: auto;
+  border: none;
+  border-radius: 32px;
+  box-shadow: 0 4px 14px 0 rgb(0 0 0 / 10%);
+  padding: 12px 32px;
+  background-color: white;
+  color: black;
+  font-weight: bold;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:focus {
+    outline: 0;
+  }
+
+  .icon {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+    vertical-align: text-bottom;
+    fill: ${({ theme }) => theme.colors.spotifyGreen};
+  }
+`;
+
 const Login = () => (
-  <main>
-    <p>
-      <a href="/api/auth/login">Sign in with Spotify</a>
-    </p>
-  </main>
+  <Layout>
+    <Link href="/api/auth/login">
+      <LoginButton>
+        <FaSpotify className="icon" />
+        Sign in with Spotify
+      </LoginButton>
+    </Link>
+  </Layout>
 );
 
 export default Login;
