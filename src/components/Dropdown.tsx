3d0292acd05ea024b73ca6fedc02038431d3a8d6
Yijiao Wang <ttwyj250@gmail.com>
2021-07-15 23:02:06 -0700

    Fix debounce issue

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,40 +1,57 @@
 import { useState } from 'react';
+import { useApolloClient } from '@apollo/client';
 import Select, { components } from 'react-select';
 import AsyncSelect from 'react-select/async';
-import { debounce } from 'lodash';
+import debounce from 'debounce-promise';
 import { FaSearch } from 'react-icons/fa';
-import { useSearch } from '../graphql/queries';
+import { SEARCH } from '../graphql/queries';
 
 const Dropdown: React.FC = () => {
-  const [search, { loading, data: searchData }] = useSearch();
+  const client = useApolloClient();
+  const [searchResults, setSearchResults] = useState([]);
   const [seeds, setSeeds] = useState([]);
 
-  const fetch = (inputValue) => {
-    search({ variables: { query: inputValue.trim() } });
+  const getAsyncOptions = async (inputValue: string): Promise<any> => {
+    return client
+      .query({
+        query: SEARCH,
+        variables: { searchTerm: inputValue.trim() },
+      })
+      .then((res) =>
+        res.data
+          ? res.data.search.map((d) => ({
+              label: d.name,
+              value: d.id,
+              artist: d.artists
+                .reduce((acc, curr) => [...acc, curr.name], [])
+                .join(', '),
+              imageUrl: d.images.find((image) => image.height === 300)?.url,
+            }))
+          : []
+      );
   };
 
-  const debouncedFetch = debounce(fetch, 1000);
+  const loadOptions = (inputValue: string) => getAsyncOptions(inputValue);
+  const debouncedLoadOptions = debounce(loadOptions, 1000);
 
   const handleSelect = (selectedOption) => {
     setSeeds({ selectedOption });
   };
 
-  const dropdownOptions =
-    searchData &&
-    searchData.search.map(({ id, name }) => ({ label: name, value: id }));
-
-  // const dropdownOptions = [
-  //   { label: 'Shark', value: 'Shark' },
-  //   { label: 'Dolphin', value: 'Dolphin' },
-  //   { label: 'Whale', value: 'Whale' },
-  //   { label: 'Octopus', value: 'Octopus' },
-  //   { label: 'Crab', value: 'Crab' },
-  //   { label: 'Lobster', value: 'Lobster' },
-  // ];
+  const formatOptionLabel = ({ value, label, artist, imageUrl }) => (
+    <div style={{ display: 'flex' }}>
+      <div style={{ width: 60, height: 60, marginRight: 10 }}>
+        <img src={imageUrl} style={{ width: '100%', height: '100%' }} />
+      </div>
+      <h5>
+        <span>{label}</span> - <span>{artist}</span>
+      </h5>
+    </div>
+  );
 
   const customStyles = {
-    control: (base, state) => ({
-      ...base,
+    control: (provided, state) => ({
+      ...provided,
       border: state.isFocused ? 0 : 0,
       boxShadow: state.isFocused ? 0 : 0,
       cursor: 'text',
@@ -43,15 +60,19 @@ const Dropdown: React.FC = () => {
       backgroundColor: 'transparent',
     }),
 
-    option: (styles, { isFocused }) => {
+    option: (provided, { isFocused }) => {
       return {
-        ...styles,
+        ...provided,
         cursor: 'pointer',
         backgroundColor: isFocused ? 'white' : 'white',
         color: isFocused ? 'rgba(255, 80, 86)' : 'black',
         lineHeight: 2,
       };
     },
+
+    input: () => ({
+      color: 'white',
+    }),
   };
 
   const DropdownIndicator = ({ children, ...rest }) => (
@@ -61,14 +82,20 @@ const Dropdown: React.FC = () => {
   );
 
   return (
-    <AsyncSelect
-      loadOptions={debouncedFetch}
-      onChange={handleSelect}
-      placeholder="Search by Track"
-      classNamePrefix="select"
-      styles={customStyles}
-      components={{ DropdownIndicator }}
-    />
+    <>
+      <AsyncSelect
+        cacheOptions
+        loadOptions={debouncedLoadOptions}
+        isMulti={true}
+        formatOptionLabel={formatOptionLabel}
+        onChange={handleSelect}
+        placeholder="Search by Track"
+        classNamePrefix="select"
+        styles={customStyles}
+        components={{ DropdownIndicator }}
+      />
+      <div>{JSON.stringify(seeds)}</div>
+    </>
   );
 };
 
