15b69ed82306410b4f5e5823a64b63f89d8bbbaf
Yijiao Wang <ttwyj250@gmail.com>
2023-01-31 08:26:45 -0800

    Add Firebase service account json file to Vercel

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -1,4 +1,3 @@
-import * as serviceAccount from '../../soundwave-service-account.json';
 import * as admin from 'firebase-admin';
 import { User } from '../apollo/entities/User';
 import { Session } from '../apollo/entities/Session';
@@ -7,6 +6,10 @@ import {
   AddTrackInput,
   RemoveTrackInput,
 } from '../apollo/entities/Track';
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+);
 
 if (admin.apps.length === 0) {
   admin.initializeApp({
